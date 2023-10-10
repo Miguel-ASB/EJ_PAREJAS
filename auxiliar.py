e192cbccecd9c7cc0f1860b9f1582537e7843fb5
mangel <9000888@alumnos.ufv.es>
2023-10-10 10:43:11 +0200

    creacion clase main y auxiliar

---
+++ auxiliar.py
@@ -0,0 +1,6 @@
+class Auxiliar:
+    def __init__(self):
+        print("Hola desde la clase Auxiliar")
+
+if __name__ == "__main__":
+    auxiliar_instance = Auxiliar()
---
+++ main.py
@@ -0,0 +1,6 @@
+class Main:
+    def __init__(self):
+        print("Hola desde la clase Main")
+
+if __name__ == "__main__":
+    main_instance = Main()
